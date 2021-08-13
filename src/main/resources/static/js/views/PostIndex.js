506c78dc548e035325f1098b0e5e387d87bd30dd
PrachiPhatak <phatak.prachi@gmail.com>
2021-08-13 10:45:32 -0500

    Add endpoint for delete

--- src/main/resources/static/js/views/PostIndex.js
+++ src/main/resources/static/js/views/PostIndex.js
@@ -1,17 +1,27 @@
+import createView from "../createView.js";
+
 export default function PostIndex(props) {
 
     return `
-        <header>
+        <header xmlns="http://www.w3.org/1999/html">
             <h1>Posts Page</h1>
         </header>
         <main>
-            <div>
-                ${props.posts.map(post => `${printOutBlogs(post)}`).join('')}  
-                <form id="post-form">
-                    <input id="title" class="border-box" name="title" type="text" placeholder="Blog Title"/>
-                    <input id="content" name="content" type="text" placeholder="Content"/>
-                    <button id="save-btn" class="btn-sm" type="submit"> Post </button>
-                </form>
+            <div class="container row row-cols-1 row-cols-sm-2 row-cols-md-2 row-cols-lg-3 g-3">
+               
+                <div class="col mb-3">
+                    <div class=" card shadow-sm p-1">
+                        <form id="post-form">
+                            <h5> New Blog </h5>
+                            <input id="title" class="border-box" name="title" type="text" placeholder="Blog Title"/>
+                            <textarea cols="20"  id="content" name="content" type="text" placeholder="Content"></textarea>
+                            <button id="save-btn" class="btn-sm" type="submit"> Post </button>
+                        </form>
+                    </div>
+                </div>
+                
+                 ${props.posts.map(post => `${printOutBlogs(post)}`).join('')}  
+                 
             </div>
         </main>
     `;
@@ -19,8 +29,13 @@ export default function PostIndex(props) {
 
 function printOutBlogs(post) {
     return `
-            <h5> ${post.title}</h5>
-            <p>${post.content}</p>
+            <div class="col mb-3">
+                <div class=" card shadow-sm p-1">
+                     <h5> ${post.title}</h5>
+                      <p>${post.content}</p>
+                      <button data-link="${post.id}" class="btn-sm delete-btn" type="submit"> Delete </button>
+                 </div>
+             </div>
     `
 }
 
@@ -42,7 +57,11 @@ export function CreatePost() {
         };
         console.log(options)
         fetch(url, options)
-            .then(data => console.log(data))
+            .then(data => {
+                console.log(data)
+                createView("/posts")
+            })
+
             .catch(error => console.error(error)); /* handle errors */
 
     });
