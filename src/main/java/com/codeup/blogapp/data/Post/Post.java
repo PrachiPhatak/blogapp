3a0fba5106e4c70d9d5831cd3bd831f4617c1c5e
PrachiPhatak <phatak.prachi@gmail.com>
2021-08-20 15:49:54 -0500

    Add persistance layer

--- src/main/java/com/codeup/blogapp/data/Post/Post.java
+++ src/main/java/com/codeup/blogapp/data/Post/Post.java
@@ -1,13 +1,28 @@
 package com.codeup.blogapp.data;
 
+import javax.persistence.*;
 import java.util.Collection;
 
+@Entity
+@Table(name = "posts")
 public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, length=120)
     private String title;
+
+    @Column(nullable = false)
     private String content;
+
+
     private User user;
     private Collection<Category> categories;
+
+    public Post() {
+    }
 
     public Post(Long id, String title, String content, User user) {
         this.id = id;
@@ -23,7 +38,6 @@ public class Post {
         this.user = user;
         this.categories = categories;
     }
-
 
 
     public Long getId() {
